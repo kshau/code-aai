b005d2439889b78cd8dd8e7332a942f397fca3b5
Keshav Shah <keshavrshah@gmail.com>
2025-01-08 22:49:42 -0500

    email verification thing

--- app/api/admin/createUser/route.ts
+++ app/api/admin/createUser/route.ts
@@ -1,9 +1,13 @@
 import { initAdmin } from "@/lib/firebase-admin/config";
+import { sendEmail } from "@/lib/sendEmail";
 import { User } from "@/lib/utils";
 import { auth, firestore } from "firebase-admin";
 import { NextRequest, NextResponse } from "next/server";
+import { randomBytes } from "crypto";
 
-const ADMIN_EMAILS = process.env.ADMIN_EMAILS ? process.env.ADMIN_EMAILS.split(',') : [];
+const ADMIN_EMAILS = process.env.ADMIN_EMAILS
+  ? process.env.ADMIN_EMAILS.split(",")
+  : [];
 
 export async function POST(request: NextRequest) {
   await initAdmin();
@@ -12,8 +16,9 @@ export async function POST(request: NextRequest) {
 
   try {
     const body = await request.json();
-    const { userToken, username, parentEmail, codingExperience, gradeLevel} = body;
-    
+    const { userToken, username, parentEmail, codingExperience, gradeLevel } =
+      body;
+
     // const adminUser = await Auth.verifyIdToken(userToken);
     // if(!adminUser || !adminUser.email || !ADMIN_EMAILS.includes(adminUser.email)){
     //   return NextResponse.json(Errors.UNAUTHORIZED);
@@ -28,22 +33,47 @@ export async function POST(request: NextRequest) {
       );
     }
 
+    const randomPassword = randomBytes(16).toString("hex"); // 16 bytes = 32 hex characters
 
     const userRecord = await Auth.createUser({
       email: `${username}@codeaai.org`,
-      password: "secure123",
+      password: randomPassword,
     });
 
+    const resetLink = await Auth.generatePasswordResetLink(
+      `${username}@codeaai.org`
+    );
+    await sendEmail(
+      parentEmail,
+      "Code AAI Account Activation: Successful Signup!",
+      `
+        Dear Parent/Guardian,\n
+    
+        We are pleased to inform you that your child, with the username "${username}", has been successfully approved to use Code AAI!\n
+    
+        To complete the registration process and set up your child's account, please follow the link below to create a secure password for their account:
+    
+        <a href="${resetLink}">Set Account Password</a>\n
+    
+        Once the password is set, your child will be able to log in and begin using the platform. You can access the official Code AAI website here: 
+        <a href="https://codeaai.org">https://codeaai.org</a>.\n
+    
+        If you have any questions or need assistance, please do not hesitate to contact us.\n
+    
+        Best regards,  
+        The Code AAI Team
+      `
+    );
 
-    const userDoc : User = {
+    const userDoc: User = {
       uid: userRecord.uid,
-      avatar:"boy1",
+      avatar: "boy1",
       username: username,
       parentEmail: parentEmail,
-      points:0,
-      solvedChallengeIds:[],
-      codingExperience:codingExperience,
-      gradeLevel:gradeLevel
+      points: 0,
+      solvedChallengeIds: [],
+      codingExperience: codingExperience,
+      gradeLevel: gradeLevel,
     };
     await Firestore.collection("users").doc(userRecord.uid).set(userDoc);
 

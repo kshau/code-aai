68fdb289a7a7bc261bf4f07ce57162e58b30556b
Keshav Shah <keshavrshah@gmail.com>
2025-02-15 17:16:09 -0500

    landing fix <3

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function Page() {
             </p>
           ))}
         </div>
-        <div className="absolute z-10 opacity-70 inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
+        <div className="absolute z-10 opacity-70 dark:opacity-10 inset-0 h-full w-full bg-background bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
       </header>
 
       <section
